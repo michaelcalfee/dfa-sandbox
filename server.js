5dfa26b8a8efce9721e791b65fdea7bb75dd299e
Michael Calfee <michael.calfee@frogdesign.com>
2018-03-20 17:47:00 -0500

    Updated server.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const path = require('path');
 
 // output folder
 app.use(express.static(path.join(__dirname, 'static/html')));
+app.use(express.static(path.join(__dirname, 'static')));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
